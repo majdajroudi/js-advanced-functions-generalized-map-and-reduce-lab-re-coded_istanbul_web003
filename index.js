047756753741865c033a9f01e54a2b1ebce40759
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-26 13:57:47 +0100

    commit tree a90d1a8a26bed67ee901150c81a0d14f6397a9c1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+
+
+function map(arr,func){
+    return arr.map(el => func(el))
+}
+
+function reduce(arr, func,start){
+    if (start === 100){
+        return arr.reduce((el, x) => func(el,x),start)
+    }
+    else {
+        return arr.reduce((el, x) => func(el,x))
+    }
+}
